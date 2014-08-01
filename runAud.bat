badbcfb29fc0d62704902814560680aa23282701
Justin C <jdc2baseball@gmail.com>
2014-08-01 12:32:49 -0400

    Added a comment in runAud.bat

--- runAud.bat
+++ runAud.bat
@@ -3,4 +3,7 @@
 ::	[1] = InFile path
 ::	[2] = DSNB outfile path/name
 ::	[3] = Vol outfile path/name
+
+:: This is a GitHub test
+
 "C:\Program Files (x86)\Python\python.exe" TMC_Aud_TextParser-V4.py InFiles/AudFiles/TMSAudit_0620_thru_0703.txt myTempOutDSNB.txt myTempOutVol.txt
